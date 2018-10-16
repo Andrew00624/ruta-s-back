5ddc9fd55ae9f96fce016c39a76892b6d08f601b
andrew <andrew00624@gmail.com>
2018-10-16 14:18:55 -0400

    Deploy

---
+++ public/precache-manifest.83477fec728073231967bcec1e7bde62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "559e812861e106280f97",
+    "url": "/static/js/main.559e8128.chunk.js"
+  },
+  {
+    "revision": "94a31ff55d934b70ac1b",
+    "url": "/static/js/1.94a31ff5.chunk.js"
+  },
+  {
+    "revision": "559e812861e106280f97",
+    "url": "/static/css/main.33e3380a.chunk.css"
+  },
+  {
+    "revision": "94a31ff55d934b70ac1b",
+    "url": "/static/css/1.b3454725.chunk.css"
+  },
+  {
+    "revision": "21c4b195782326dd41237ed193094edc",
+    "url": "/index.html"
+  }
+];
